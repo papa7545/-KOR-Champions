3669d951eda975f6f2dda4a935afd2f5650455aa
X3n- <hello.x3n@gmail.com>
2015-01-12 18:25:22 +0900

    Kor_AIO

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,35 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// 어셈블리의 일반 정보는 다음 특성 집합을 통해 제어됩니다.
-// 어셈블리와 관련된 정보를 수정하려면
-// 이 특성 값을 변경하십시오.
-[assembly: AssemblyTitle("JeonChampions")]
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Kor_AIO")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft Corporation")]
-[assembly: AssemblyProduct("JeonChampions")]
-[assembly: AssemblyCopyright("Copyright © Microsoft Corporation 2015")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Kor_AIO")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// ComVisible을 false로 설정하면 이 어셈블리의 형식이 COM 구성 요소에 
-// 표시되지 않습니다.  COM에서 이 어셈블리의 형식에 액세스하려면 
-// 해당 형식에 대해 ComVisible 특성을 true로 설정하십시오.
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("af1b64b0-0a07-4ac4-a041-81145892f930")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("323f150b-db06-4a6d-bee0-27ef08405fc2")]
 
-// 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
+// Version information for an assembly consists of the following four values:
 //
-//      주 버전
-//      부 버전 
-//      빌드 번호
-//      수정 버전
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
 //
-// 모든 값을 지정하거나 아래와 같이 '*'를 사용하여 빌드 번호 및 수정 버전이 자동으로
-// 지정되도록 할 수 있습니다.
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
